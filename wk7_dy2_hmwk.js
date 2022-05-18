ac9dabb18e7fa036053015886a424fc952de6175
Dakodah Bradley <comatose9592@gmail.com>
2022-05-18 11:25:21 -0500

    first commit

---
+++ wk7_dy2_hmwk.js
@@ -0,0 +1,37 @@
+//==================Exercise #1 ==========//
+/* Write a function that takes in the string and the list of dog names
+ loop through the list and check that the current name is in the string passed in. 
+ The output should be:
+ "Matched dog_name" if name is in the string, if no matches are present console.log "No Matches"
+*/
+
+function findWords(){
+    let dog_string = "Hello Max, my name is Dog, and I have purple eyes!";
+    if (dog_string.includes("Max","HAS","PuRple","dog")) {
+        return "Matched";
+    } else {
+        return "No Matches"
+    };
+        
+
+};
+console.log(findWords())
+
+//Call method here with parameters
+
+//============Exercise #2 ============//
+/*Write a fucntion that takes in an array and removes every even index with a splice,
+and replaces it with the string "even index" */
+
+const arr = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+
+function replaceEvens(arr){
+    arr.splice(0,1, "even index")
+    return arr
+ };
+
+console.log(replaceEvens(arr))
+
+//Expected output
+//Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+//Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
